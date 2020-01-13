4646a8b97b173e37b70559c6d97572b180899be4
karla <karla.jeria@gmail.com>
2020-01-13 18:18:33 -0300

    primera subida de archivos

---
+++ assets/js.js
@@ -0,0 +1,8 @@
+
+function informacion() {
+  document.getElementById("info").innerHTML = "gracias por comunicarte, pronto estaremos en contacto";
+}
+
+  $(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+  });
